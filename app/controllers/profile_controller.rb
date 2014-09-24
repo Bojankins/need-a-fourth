5770933605e02f66d5cb4b0d3b66fc8bf4071c02
BoJankins <Rs_Bo_Jankins@yahoo.com>
2014-09-23 20:54:56 -0500

    fixed some stying issues

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -7,7 +7,7 @@ class ProfileController < ApplicationController
   respond_to :html, :json
 
   def update
-    @profile = Profile.find(params[:id])
+    @profile = Profile.find(params[:user_id])
     @Profile.update_attributes(params[:profile])
     respond_with @profile
   end
