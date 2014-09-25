def8697aa1aab9a27f62620d960d32c8885b89c8
BoJankins <Rs_Bo_Jankins@yahoo.com>
2014-09-25 10:32:50 -0500

    set up s3 for carrierwave

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,9 +1,9 @@
-CarrierWave.configure do |config|
-  config.fog_credentials = {
-      :provider               => 'AWS',
-      :aws_access_key_id      => 'AKIAI2ANTURSR7DGAZOA',
-      :aws_secret_access_key  => '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'
-      # :region                 => ENV['S3_REGION'] # Change this for different AWS region. Default is 'us-east-1'
-  }
-  config.fog_directory  = 'kentucky89'
-end
+# CarrierWave.configure do |config|
+#   config.fog_credentials = {
+#       :provider               => 'AWS',
+#       :aws_access_key_id      => 'AKIAI2ANTURSR7DGAZOA',
+#       :aws_secret_access_key  => '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'
+#       # :region                 => ENV['S3_REGION'] # Change this for different AWS region. Default is 'us-east-1'
+#   }
+#   config.fog_directory  = 'kentucky89'
+# end
