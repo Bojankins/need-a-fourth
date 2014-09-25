ba9d82f84d5cf68546331a3189957bdb23876c89
BoJankins <Rs_Bo_Jankins@yahoo.com>
2014-09-25 10:22:42 -0500

    set up s3 for carrierwave

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,7 +1,7 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
       :provider               => 'AWS',
-      :aws_access_key_id      => 'AKIAI2ANTURSR7DGAZOA'
+      :aws_access_key_id      => 'AKIAI2ANTURSR7DGAZOA',
       :aws_secret_access_key  => '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'
       # :region                 => ENV['S3_REGION'] # Change this for different AWS region. Default is 'us-east-1'
   }
